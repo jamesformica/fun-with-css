712add8bc67dd3d8db30dbb9b9988231852f2465
James Formica <james.formica@gmail.com>
2016-06-24 07:39:59 +1000

    yeh yeh functions in functions whatever...

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,50 +1,52 @@
 function InitialiseDemo() {
+	
 	// initialising fullpage js
 	$("#fullpage").fullpage({
 		controlArrows: false,
 		slidesNavigation: true
 	});
 
+	// hookup events
 	$(".ui-code").on("keyup", SomeonePressedEnter);
 	$(".ui-code").on("input propertychange", SomeoneEnteredSomething);
 	$(".ui-solution").on("click", SomeoneGaveUpOrWantsToSeeTheAnswer);
+}
 
-	function SomeoneEnteredSomething(e) {
+function SomeoneEnteredSomething(e) {
+	var $curCode = $(e.currentTarget);
+	var $board = $curCode.closest(".ui-board");
+	var $style = $board.children("style");
+
+	$style.html($curCode.val());
+}
+
+function SomeonePressedEnter(e) {
+	if (e.keyCode === 13) {
 		var $curCode = $(e.currentTarget);
-		var $board = $curCode.closest(".ui-board");
-		var $style = $board.children("style");
+		var curCodeHtml = $curCode.get(0);
 
-		$style.html($curCode.val());
+		var cursorPos = curCodeHtml.selectionStart;
+		var textAreaTxt = $curCode.val();
+		var indentedText = textAreaTxt.substring(0, cursorPos) + "  " + textAreaTxt.substring(cursorPos);
+		
+		$curCode.val(indentedText);
+
+		var newCursorPos = cursorPos + 2;
+		curCodeHtml.setSelectionRange(newCursorPos, newCursorPos);
 	}
+}
 
-	function SomeonePressedEnter(e) {
-		if (e.keyCode === 13) {
-			var $curCode = $(e.currentTarget);
-			var curCodeHtml = $curCode.get(0);
+function SomeoneGaveUpOrWantsToSeeTheAnswer(e) {
+	var $curSolution = $(e.currentTarget);
 
-			var cursorPos = curCodeHtml.selectionStart;
-			var textAreaTxt = $curCode.val();
-			var indentedText = textAreaTxt.substring(0, cursorPos) + "  " + textAreaTxt.substring(cursorPos);
-			
-			$curCode.val(indentedText);
+	var solution = $curSolution.attr("solution");
 
-			var newCursorPos = cursorPos + 2;
-			curCodeHtml.setSelectionRange(newCursorPos, newCursorPos);
-		}
-	}
+	var $board = $curSolution.closest(".ui-board");
+	var $code = $board.children(".ui-code");
 
-	function SomeoneGaveUpOrWantsToSeeTheAnswer(e) {
-		var $curSolution = $(e.currentTarget);
+	var colour = $board.attr("colour");
+	solution = solution.replace(/{background}/g, colour);
 
-		var solution = $curSolution.attr("solution");
-
-		var $board = $curSolution.closest(".ui-board");
-		var $code = $board.children(".ui-code");
-
-		var colour = $board.attr("colour");
-		solution = solution.replace(/{background}/g, colour);
-
-		$code.val(solution);
-		$code.trigger("input");
-	}
+	$code.val(solution);
+	$code.trigger("input");
 }
